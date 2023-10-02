9710b53b0fd6ab07de108b555c26a429930ecce4
Robert Klink <roberthklink@gmail.com>
2023-10-02 11:57:44 +0200

    add: fedora post install script 1

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# Gnome config
-My config files for my Gnome setup.
+# Gnome/Fedora config
+My config files for my Gnome/Fedora setup.
 
 ### Links
 https://www.fontsquirrel.com/fonts/aurulent-sans-mono  
 https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme  
+
+### Scripts
+There are a couple of scripts. If you're running Fedora and Gnome you can just run like so:
+```
+sh fedora-1.sh
+# it will reboot, 2nd scripts neede
+sh fedora-2.sh
+```
+
+
---
+++ fedora-1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# A simple script to do some post install of fedora
+#     Rombertus
+
+# add some options to dnf to speed it up
+echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
+
+# install zsh and oh-my-zsh
+sudo dnf install zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# update
+sudo dnf upgrade --refresh
+sudo dnf check
+sudo dnf autoremove
+sudo fwupdmgr get-devices
+sudo fwupdmgr refresh --force
+sudo fwupdmgr get-updates
+sudo fwupdmgr update
+sudo reboot now
